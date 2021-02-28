175947b38e60a949b566e5abe7e33e3cb1905155
romero-c-raul <raulromerocano@aggienetwork.com>
2021-02-28 14:37:41 -0330

    Implemented 'data_path' method that returns the correct path to where documents will be stored based on the current environment. Code was refactored to make use of this method

--- cms.rb
+++ cms.rb
@@ -10,16 +10,6 @@ configure do
   set :session_secret, 'super secret'
 end
 
-root = File.expand_path("..", __FILE__)
-
-get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
-    File.basename(path)
-  end
-
-  erb :index
-end
-
 def render_markdown(content)
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   markdown.render(content)
@@ -37,8 +27,27 @@ def load_file_content(file_path)
   end
 end
 
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
+
+root = File.expand_path("..", __FILE__)
+
+get "/" do
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
+    File.basename(path)
+  end
+
+  erb :index
+end
+
 get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   if File.file?(file_path)
     load_file_content(file_path)
@@ -49,7 +58,7 @@ get "/:filename" do
 end
 
 get "/:filename/edit" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   @filename = params[:filename]
   @content = File.read(file_path)
@@ -58,9 +67,10 @@ get "/:filename/edit" do
 end
 
 post "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   File.write(file_path, params[:content])
+  
   session[:message] = "#{params[:filename]} has been updated."
   redirect "/"
 end
